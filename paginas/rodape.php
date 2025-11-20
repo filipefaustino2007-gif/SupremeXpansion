14fa7a67039f0b32ea979a715878d5210f1d5c9d
filipefaustino2007-gif <filipefaustino2007@gmail.com>
2025-11-20 16:54:15 +0000

    Add files via upload

---
+++ paginas/rodape.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/rodape.css">
+</head>
+<body>
+
+<footer class="main-footer">
+    <div class="footer-content">
+
+        <!-- Coluna Esquerda -->
+        <div class="footer-left">
+            <div class="footer-logo">
+                <img src="../img/logo_branco.svg" alt="SupremeXpansion Logo">
+            </div>
+            <div class="footer-text">
+                <p>2025 © SupremeXpansion - Capturamos a Realidade</p>
+                <p>Todos os direitos reservados |<a href="politica_privacidade.php" target="_blank" rel="noopener noreferrer"> Política de Privacidade</a></p>
+            </div>
+            <div class="footer-socials">
+                <a href="https://www.facebook.com/supremexpansion2023/" target="_blank" rel="noopener noreferrer">
+                    <img src="../img/facebook.svg" alt="Facebook">
+                </a>
+                <a href="https://www.instagram.com/supremexpansion/" target="_blank" rel="noopener noreferrer">
+                    <img src="../img/instagram.svg" alt="Instagram">
+                </a>
+                <a href="https://www.linkedin.com/company/supremexpansion" target="_blank" rel="noopener noreferrer">
+                    <img src="../img/linkedin.svg" alt="LinkedIn">
+                </a>
+            </div>
+        </div>
+
+        <!-- Coluna Central -->
+        <div class="footer-middle">
+            <p class="servicos">Serviços</p>
+            <br>
+            <a href="servico1.php">Laser Scan e Levantamentos 3D</a>
+            <br>
+            <a href="servico2.php">UAV/Drone Fotogrametria</a>
+            <br>
+            <a href="servico3.php">Desenho 2D e Modelação 3D</a>
+            <div class="footer-logo3d">
+                <a href="http://3dscan2cad.com/" target="_blank" rel="noopener noreferrer">
+                <img src="../img/logo3d.png" alt="3DScan2CAD">
+                </a>
+            </div>
+        </div>
+
+        <!-- Coluna Direita -->
+        <div class="footer-right">
+            <p class="servicos">Contactos</p>
+            <br>
+            <p>Telefone</p>
+            <p>(Chamada para a rede móvel nacional)</p>
+            <p>PT +351 935 584 011</p>
+            <p>UK +44 7557 795 968</p>
+            <br>
+            <p>Email</p>
+            <p>info@supremexpansion.com</p>
+        </div>
+
+    </div>
+</footer>
+
+</body>
+</html>
